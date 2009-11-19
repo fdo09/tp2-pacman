292c36b10ea754202afbda31419701d7d27d8a5c
elfede521 <elfede521@91591542-cae0-11de-b743-054a4f195298>
2009-11-19 00:37:14 +0000

    creados algunos atributos del pac. y creado el metodo mover

--- src/modelo/Personaje.java
+++ src/modelo/Personaje.java
@@ -1,5 +1,19 @@
 package modelo;
 
-public interface Personaje {
-
+public abstract class Personaje {
+	
+	int posicionX;
+	int posicionY;
+	int direccionX;
+	int direccionY;
+	
+	public void mover() {
+		if (direccionX == 0 | direccionY == 0) {
+			posicionX += direccionX;
+			posicionY += direccionY;
+		}
+		else
+			//definir que excepcion lanzar. esto es para que el pac no se pueda mover en diagonal
+			throw new RuntimeException();
+	}
 }
