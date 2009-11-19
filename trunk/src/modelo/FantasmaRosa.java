468a55caa911fc1600e2ef1b70cdfd9c438731b8
julian20dambrosio <julian20dambrosio@91591542-cae0-11de-b743-054a4f195298>
2009-11-19 03:10:04 +0000

    Clase abstracta Fantasma creada con sus respectivas hijas. Modificaciones en clase Personaje y clase Pac. Probando que se ejecute bien el commit ya que muestra error.

---
+++ trunk/src/modelo/FantasmaRosa.java
@@ -0,0 +1,49 @@
+package modelo;
+
+public class FantasmaRosa extends Fantasma {
+
+	private String estado;
+	private boolean fueComido;
+	private int posicionEnX;
+	private int posicionEnY;
+	
+	public FantasmaRosa(int coordenadaEnX, int coordenadaEnY) {
+		super(coordenadaEnX, coordenadaEnY);
+						
+	}
+
+	public void mover() {
+		
+		//Ver si hay otra forma de hacer esto.
+		
+		if(this.estado == "Deambular"){
+			
+			this.deambular();
+		}
+		if(this.estado == "Huir"){
+			
+			this.huir();
+		}
+		if(this.estado == "Atrapar"){
+			
+			this.atrapar();
+		}
+
+	}
+
+	private void huir() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void atrapar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void deambular() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- trunk/src/modelo/Personaje.java
+++ trunk/src/modelo/Personaje.java
@@ -2,18 +2,10 @@ package modelo;
 
 public abstract class Personaje {
 	
-	int posicionX;
-	int posicionY;
-	int direccionX;
-	int direccionY;
-	
-	public void mover() {
-		if (direccionX == 0 | direccionY == 0) {
-			posicionX += direccionX;
-			posicionY += direccionY;
-		}
-		else
-			//definir que excepcion lanzar. esto es para que el pac no se pueda mover en diagonal
-			throw new RuntimeException();
-	}
+	private String estado;
+	private boolean fueComido;
+	private int posicionEnX;
+	private int posicionEnY;
+
+	public abstract void mover();
 }
