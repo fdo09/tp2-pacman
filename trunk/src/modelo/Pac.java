4e77470dfc76f6301ec050e5a24f058f5ea73a95
elfede521 <elfede521@91591542-cae0-11de-b743-054a4f195298>
2009-11-19 00:37:14 +0000

    creados algunos atributos del pac. y creado el metodo mover

--- trunk/src/modelo/Pac.java
+++ trunk/src/modelo/Pac.java
@@ -1,9 +1,11 @@
 package modelo;
 
-public class Pac implements Personaje{
+public class Pac extends Personaje{
 
 	int posicionX;
 	int posicionY;
+	int direccionX;
+	int direccionY;
 	boolean power;
 	
 	public Pac (int posicionX, int posicionY){
@@ -11,4 +13,5 @@ public class Pac implements Personaje{
 		this.posicionX = posicionX;
 		this.posicionY = posicionY;
 	}
+
 }
