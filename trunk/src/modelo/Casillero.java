80902afee44e9bf898fedad8d15c81dac2110bac
elfede521 <elfede521@91591542-cae0-11de-b743-054a4f195298>
2009-11-21 21:58:57 +0000

    Desarrolle los metodos moverDerecha,etc para hacer el movimiento del personaje. Alguno de estos metodos va a ser llamado por "mover" luego de definir la direccion del movimiento de acuerdo la estrategia (en el caso del fantasma) o el teclado (en el caso del pac)

--- trunk/src/modelo/Casillero.java
+++ trunk/src/modelo/Casillero.java
@@ -1,5 +1,9 @@
 package modelo;
 
 public interface Casillero {
-
+	/* cada casillero puede tener un atributo objeto donde se almacena el objeto. Y un atributo
+	 * personaje donde se almacena el casillero. Esto es para solucionar el proble de que en un
+	 * mismo casillero puede haber un fantasma y una semilla. Hay que definir el diseño de los
+	 * mensajes entre un personaje y un elemento al es pisar un casillero
+	 */
 }
