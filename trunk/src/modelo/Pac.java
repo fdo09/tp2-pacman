66173c1f5a06895fe0771c14d2ca73335aefc6fc
julian20dambrosio <julian20dambrosio@91591542-cae0-11de-b743-054a4f195298>
2009-11-21 22:03:16 +0000

    Creación de la clase Pac, además se agrega a clase Tablero el metodo hayObstaculo.

--- trunk/src/modelo/Pac.java
+++ trunk/src/modelo/Pac.java
@@ -1,26 +1,106 @@
 package modelo;
 
+import java.awt.Point;
 
+enum Direcciones{IZQUIERDA, ARRIBA, DERECHA, ABAJO};
 public class Pac implements Personaje{
 
+
+	private Tablero tablero;
+	private Point posicion;
+	private Direcciones estado;
 	
-	private boolean fueComido;
-	private int posicionEnX;
-	private int posicionEnY;
-	
-	
-	public Pac (int coordenadaEnX, int coordenadaEnY){
+	public Pac (Tablero tablero, Point posicion){
 		
-		this.fueComido = false;
-		this.posicionEnX = coordenadaEnX;
-		this.posicionEnY = coordenadaEnY;
+		
+		
+		this.tablero = tablero;
+		this.posicion = posicion;
+		this.estado = Direcciones.DERECHA;
+	}
+	
+	
+	public void vivir(){
+		
+		this.mover();
+		
 	}
 
 	public void mover() {
+		
+		switch (this.estado){
+		
+				case IZQUIERDA:
+					this.desplazarseEnX(-1);
+				case ARRIBA:
+					this.desplazarseEnY(1);
+				case DERECHA:
+					this.desplazarseEnX(1);
+				case ABAJO:
+					this.desplazarseEnY(-1);
+				}
+		
+		}
 
-		/* Depende de la tecla presionada, el Pac se movera en determinada direccion
-		 * siempre y cuando el movimiento sea posible.*/
+	public void desplazarseEnX(int unValor){
+		
+		boolean posicionValidaEnX;
+		posicionValidaEnX = this.validarPosicionEnX(unValor);
+		
+		if(posicionValidaEnX){
+					
+			this.posicion.setLocation(this.posicion.getX()+ unValor, (this.posicion.getY()));
+		}
+	}
+	
+	public void desplazarseEnY(int unValor){
+		boolean posicionValidaEnY;
+		
+		posicionValidaEnY = this.validarPosicionEnY(unValor);
+		
+		if(posicionValidaEnY)
+			{
+			this.posicion.setLocation(this.posicion.getX(), (this.posicion.getY() + unValor));
+			}
 		
 	}
 
+	private boolean validarPosicionEnX(int unValor) {
+
+		Point unaPosicion = new Point();
+		
+		unaPosicion.setLocation(this.posicion.getX()+ unValor, (this.posicion.getY()));
+		
+		return hayObstaculo(unaPosicion);
+		
+	}
+	
+	private boolean validarPosicionEnY(int unValor) {
+
+		Point unaPosicion = new Point();
+		
+		unaPosicion.setLocation(this.posicion.getX(), (this.posicion.getY()+ unValor));
+		
+		return hayObstaculo(unaPosicion);
+	}
+
+
+	private boolean hayObstaculo(Point unaPosicion) {
+		if(tablero.hayObstaculo(unaPosicion)){
+			return false;
+		}return true;
+	}
+	
+	
+	public int getX() {
+		// TODO Auto-generated method stub
+		return (int) posicion.getX();
+	}
+
+
+	public int getY() {
+		// TODO Auto-generated method stub
+		return (int) posicion.getY();
+	}
+	
 }
