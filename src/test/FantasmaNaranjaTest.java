565a460c98dc8d268202c46a0955737e62a7acf4
julian20dambrosio <julian20dambrosio@91591542-cae0-11de-b743-054a4f195298>
2009-11-22 19:05:06 +0000

    Creación de Test, y un nuevo fantasma, FantasmaAmarillo, ya que nuestro tutor nos dijo que hagamos cinco.

---
+++ src/test/FantasmaNaranjaTest.java
@@ -0,0 +1,20 @@
+package test;
+
+import junit.framework.TestCase;
+
+public class FantasmaNaranjaTest extends TestCase {
+
+	
+	public void testHuir(){
+		   	
+		
+	}
+	
+	public void testregresar() {
+		
+	}
+	
+	public void testAtrapar(){
+		
+	}	
+}
