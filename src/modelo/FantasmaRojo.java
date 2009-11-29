6f41ef77328a885a8376063ce0080dd23e4eff4e
elfede521 <elfede521@91591542-cae0-11de-b743-054a4f195298>
2009-11-29 21:40:03 +0000

    Refactorice el metodo mover del fantasmaRojo

--- src/modelo/FantasmaRojo.java
+++ src/modelo/FantasmaRojo.java
@@ -5,53 +5,32 @@ import java.util.Collection;
 
 import fijos.*;
 
+enum Inteligencias{INTELIGENTE, TONTO};
+
 public class FantasmaRojo extends Fantasma {
 
 	private Tablero tablero;
 	private Estados estado;
 	private Punto posicion;
+	private int contadorInteligencia;
+	private Inteligencias intel;
+	public static final int CAMBIO_INTELIGENCIA = 20;
+	public static final int CICLO_INTELIGENCIA = 40;
 	
 	
 	public FantasmaRojo(Tablero tablero, Punto posicion) {
 		super(tablero, posicion);
 		this.estado = Estados.ATRAPAR;
+		this.intel = Inteligencias.INTELIGENTE;
 	}
 
-	
+		
 	public void mover(){
-		
-		Collection<Punto> adjacentesValidos = this.tablero.obtenerAdjacentesValidos(this.posicion);
-		
-		int contador = 0;
-		int tiempoCambioDeEstado = 20;
-		
-		if(contador <= tiempoCambioDeEstado){
-			
-			this.estrategiaMiedosa(adjacentesValidos, contador);
-			
-		}
-		
-		else{
-			
-			this.estrategiaValiente(adjacentesValidos, contador);
-
-			int tiempoDeUnNuevoCambio = 40;
-			
-			if (contador == tiempoDeUnNuevoCambio){
-				
-				contador = 0;
-			}
-		}
-		
-	}
-
-
-	private void estrategiaValiente(Collection<Punto> adjacentesValidos,int contador) {
-		
 		Punto nuevaPosicion;
+		Collection<Punto> adjacentesValidos = this.tablero.obtenerAdjacentesValidos(this.posicion);
+		this.definirInteligencia();
 		
 		switch (this.estado){
-		
 		case ATRAPAR:
 			nuevaPosicion = this.calcularAtrapada(adjacentesValidos);
 			this.posicion = nuevaPosicion;
@@ -64,57 +43,41 @@ public class FantasmaRojo extends Fantasma {
 			nuevaPosicion = this.calcularRegreso(adjacentesValidos);
 			this.posicion = nuevaPosicion;
 		}
-		contador++;
+	}
+
+
+	private void definirInteligencia() {
+		this.contadorInteligencia++;
 		
-	}
-
-
-	private void estrategiaMiedosa(Collection<Punto> adjacentesValidos,int contador) {
-	
-		Punto nuevaPosicion;
+		if(this.contadorInteligencia == CICLO_INTELIGENCIA)
+			this.contadorInteligencia = 0;
 		
-		switch (this.estado)
-		{
-		case ATRAPAR:
-			nuevaPosicion = this.calcularAtrapadaConMiedo(adjacentesValidos);
-			this.posicion = nuevaPosicion;
-			this.comer();
-		case HUIR:
-			nuevaPosicion = this.calcularHuidaConMiedo(adjacentesValidos);	
-			this.posicion = nuevaPosicion;
-			this.serComido();
-		case COMIDO:
-			nuevaPosicion = this.calcularRegreso(adjacentesValidos);
-			this.posicion = nuevaPosicion;
-		}
-		contador++;
-	}
-	
-	private Punto calcularAtrapadaConMiedo(Collection<Punto> adjacentesValidos) {
-		Punto posicionPacman = tablero.obtenerPacman().obtenerPosicion();
-		ArrayDeque<Punto> pila = posicionPacman.obtenerPosicionesOrdenadas(adjacentesValidos);
-		return pila.peekLast();
+		else if(this.contadorInteligencia <= CAMBIO_INTELIGENCIA)
+			this.intel = Inteligencias.INTELIGENTE;
+			
+		else
+			this.intel = Inteligencias.TONTO;
 	}
 
-
-	private Punto calcularHuidaConMiedo(Collection<Punto> adjacentesValidos) {
-		Punto posicionPacman = tablero.obtenerPacman().obtenerPosicion();
-		ArrayDeque<Punto> pila = posicionPacman.obtenerPosicionesOrdenadas(adjacentesValidos);
-		return pila.peekFirst();
-	} 
-	
 	
 	protected Punto calcularAtrapada(Collection<Punto> adjacentesValidos) {
 		Punto posicionPacman = tablero.obtenerPacman().obtenerPosicion();
 		ArrayDeque<Punto> pila = posicionPacman.obtenerPosicionesOrdenadas(adjacentesValidos);
-		return pila.peekFirst();
+		if(this.intel == Inteligencias.INTELIGENTE)
+			return pila.peekFirst();
+		else
+			return pila.peekLast();
+
 	}
 
 
 	protected Punto calcularHuida(Collection<Punto> adjacentesValidos) {
 		Punto posicionPacman = tablero.obtenerPacman().obtenerPosicion();
 		ArrayDeque<Punto> pila = posicionPacman.obtenerPosicionesOrdenadas(adjacentesValidos);
-		return pila.peekLast();
+		if(this.intel == Inteligencias.INTELIGENTE)
+			return pila.peekLast();
+		else
+			return pila.peekFirst();
 	}
 
 }
