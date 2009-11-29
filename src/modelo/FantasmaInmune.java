7215ddf8f1cb051bbf7f9ba424a1d381628e6ff5
elfede521 <elfede521@91591542-cae0-11de-b743-054a4f195298>
2009-11-29 21:14:03 +0000

    Desarrolle la estrategia para huir y atrapar para el FantasmaInmune

--- src/modelo/FantasmaInmune.java
+++ src/modelo/FantasmaInmune.java
@@ -2,11 +2,16 @@ package modelo;
 
 
 
+import java.util.ArrayDeque;
 import java.util.Collection;
+import java.util.Random;
+
 import fijos.*;
 
 public class FantasmaInmune extends Fantasma  {
 
+    public static final int VISION = 10;
+    Punto destino;
 	private Tablero tablero;
 	private Punto posicion;
 	private Estados estado;
@@ -21,17 +26,58 @@ public class FantasmaInmune extends Fantasma  {
 
 	@Override
 	protected Punto calcularAtrapada(Collection<Punto> adjacentesValidos) {
-		// TODO Auto-generated method stub
-		return null;
+			Punto posicionPacman = tablero.obtenerPacman().obtenerPosicion();
+			double distanciaPacman = this.posicion.distancia(posicionPacman);
+			
+			if(distanciaPacman < VISION){
+				posicionPacman = tablero.obtenerPacman().obtenerPosicion();
+				ArrayDeque<Punto> pila = posicionPacman.obtenerPosicionesOrdenadas(adjacentesValidos);
+				return pila.peekFirst();
+			}
+			else{
+				if(this.posicion.equals(destino))
+					this.calcularPosicionDestino();
+				
+				ArrayDeque<Punto> pila = destino.obtenerPosicionesOrdenadas(adjacentesValidos);
+				return pila.peekFirst();
+			}
+	}
+
+
+	private void calcularPosicionDestino() {
+		Punto dimension = tablero.obtenerDimension();
+		Random generadorX = new Random(dimension.getPuntoX());
+		Random generadorY = new Random(dimension.getPuntoY());
+		Punto posicionAux;
+		do{
+			int numAzarX = generadorX.nextInt();
+			int numAzarY = generadorY.nextInt();
+			posicionAux = new Punto(numAzarX, numAzarY);
+		} while (!tablero.esTransitable(posicionAux) || posicionAux == this.posicion);
+		
+		this.posicion = posicionAux;
 	}
 
 
 	@Override
 	protected Punto calcularHuida(Collection<Punto> adjacentesValidos) {
-		// TODO Auto-generated method stub
-		return null;
+		Punto posicionPacman = tablero.obtenerPacman().obtenerPosicion();
+		double distanciaPacman = this.posicion.distancia(posicionPacman);
+		
+		if(distanciaPacman < VISION){
+			posicionPacman = tablero.obtenerPacman().obtenerPosicion();
+			ArrayDeque<Punto> pila = posicionPacman.obtenerPosicionesOrdenadas(adjacentesValidos);
+			return pila.peekLast();
+		}
+		else{
+			if(this.posicion.equals(destino))
+				this.calcularPosicionDestino();
+			
+			ArrayDeque<Punto> pila = destino.obtenerPosicionesOrdenadas(adjacentesValidos);
+			return pila.peekLast();
+		}
 	}
-
+	
 	
 	public boolean esComible(){
 		//EL fantasmaInmune nunca puede ser comido
