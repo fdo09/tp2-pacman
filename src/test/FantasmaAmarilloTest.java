c5ab096b7c59037defdae510ff124ed997cac586
julian20dambrosio <julian20dambrosio@91591542-cae0-11de-b743-054a4f195298>
2009-12-05 21:35:16 +0000

    Comenzando el test del fantasmaAmarillo

--- src/test/FantasmaAmarilloTest.java
+++ src/test/FantasmaAmarilloTest.java
@@ -7,50 +7,82 @@ import modelo.fijos.Punto;
 import modelo.fijos.Tablero;
 import modelo.moviles.Fantasma;
 import modelo.moviles.FantasmaAmarillo;
-import modelo.moviles.FantasmaAzul;
-import modelo.moviles.FantasmaInmune;
-import modelo.moviles.FantasmaNaranja;
-import modelo.moviles.FantasmaRojo;
 import modelo.moviles.Pacman;
 
 public class FantasmaAmarilloTest extends TestCase {
 
 	private Tablero tab;
-	private Fantasma fantasmaAzul;
-	private Fantasma fantasmaRojo;
-	private Fantasma fantasmaNaranja;
+	
 	private Fantasma fantasmaAmarillo;
-	private Fantasma fantasmaInmune;
+	
 	private Pacman pacman;
 
 	protected void setUp() throws Exception {
 		
 		   CargaTablero cargador = new CargaTablero();
 		   tab = cargador.cargar("xml/tablero.xml");
-		   Punto puntoAzul = new Punto(15,12);
-		   Punto puntoRojo = new Punto(15,13);
-		   Punto puntoNaranja = new Punto(15,14);
-		   Punto puntoAmarillo = new Punto(15,15);
-		   Punto puntoInmune = new Punto(13,12);
-		   fantasmaAzul = new FantasmaAzul(tab, puntoAzul);
-		   fantasmaRojo = new FantasmaRojo(tab, puntoRojo);
-		   fantasmaNaranja = new FantasmaNaranja(tab, puntoNaranja);
+		 
+		   Punto puntoAmarillo = new Punto(4,3);
+		 
+		  
 		   fantasmaAmarillo = new FantasmaAmarillo(tab, puntoAmarillo);
-		   fantasmaInmune = new FantasmaInmune(tab, puntoInmune);
-		   tab.addFantasma(fantasmaAzul);
-		   tab.addFantasma(fantasmaRojo);
-		   tab.addFantasma(fantasmaNaranja);
+		 
 		   tab.addFantasma(fantasmaAmarillo);
-		   tab.addFantasma(fantasmaInmune);
-		   Punto puntoPacman = new Punto(29,10);
+		 
+		   Punto puntoPacman = new Punto(7,3);
+		   
 		   pacman = new Pacman(tab, puntoPacman);
+		   
 		   tab.addPacman(pacman);
+		   
 		   super.setUp();
 			
 		}
 	
 	public void testAtrapar(){
-		assertFalse(fantasmaAmarillo.esComible());
+		
+		
+		Punto posicionEsperada = new Punto (4,2);
+		
+		fantasmaAmarillo.mover();
+		
+		assertEquals(posicionEsperada, fantasmaAmarillo.getPosicion());
+		
+		//---------
+		
+		posicionEsperada.moverHaciaAbajo();
+		
+		fantasmaAmarillo.mover();
+		
+		assertEquals(posicionEsperada, fantasmaAmarillo.getPosicion());
+		
+		//-----------
+		
+		posicionEsperada.moverHaciaAbajo();
+		
+		fantasmaAmarillo.mover();
+		
+		assertEquals(posicionEsperada, fantasmaAmarillo.getPosicion());
+		
+		//-----------
+		
+		
+		posicionEsperada.moverHaciaAbajo();
+		
+		fantasmaAmarillo.mover();
+		
+		assertEquals(posicionEsperada, fantasmaAmarillo.getPosicion());
+		
+		//-----------
+		
+		posicionEsperada.moverHaciaDerecha();
+		
+		fantasmaAmarillo.mover();
+		
+		assertEquals(posicionEsperada, fantasmaAmarillo.getPosicion());
+		
+		//-----------
+		
 		
 	}	
 	
