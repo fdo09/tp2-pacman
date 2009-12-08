f828d9da271e507eb5810d074152076d798eae4f
mrodriguez861 <mrodriguez861@91591542-cae0-11de-b743-054a4f195298>
2009-12-08 14:57:06 +0000

    Vista del tablero, ademas he agregado un nuevo xml para que se vea bien el tablero.

---
+++ trunk/src/modelo/vista/PosicionTablero.java
@@ -0,0 +1,57 @@
+package modelo.vista;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import ar.uba.fi.algo3.titiritero.Posicionable;
+
+public class PosicionTablero implements Posicionable, Iterable<Integrante>{
+
+private int alto;
+private int ancho;
+private List<Integrante> integrantes;
+
+	public PosicionTablero(int alto, int ancho){
+		this.alto = alto;
+		this.ancho = ancho;
+		this.integrantes = new ArrayList<Integrante>();
+	}
+
+	public int getX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public Iterator<Integrante> iterator() {
+		// TODO Auto-generated method stub
+		return this.integrantes.iterator();
+	}
+	
+	public void agregarIntegrante(Integrante integrante){
+		     this.integrantes.add(integrante);
+	}
+	
+	public void setAlto(int alto){
+		this.alto = alto;
+	}
+	
+	public void setAncho(int ancho){
+		this.ancho = ancho;
+	}
+	
+	public int getAlto(){
+		return this.alto;
+	}
+	
+	public int getAncho(){
+		return this.ancho;
+	}
+	
+	
+}
---
+++ trunk/src/modelo/vista/VentanaPrincipal.java
@@ -0,0 +1,47 @@
+package modelo.vista;
+
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import ar.uba.fi.algo3.titiritero.ControladorJuego;
+import ar.uba.fi.algo3.titiritero.SuperficieDeDibujo;
+import ar.uba.fi.algo3.titiritero.vista.Panel;
+
+public class VentanaPrincipal extends Frame {
+
+	public VentanaPrincipal(ControladorJuego unControladorJuego) {
+		this.controladorJuego = unControladorJuego;
+		this.setTitle("Tablero PACMAN");
+		// this.setSize(300, 300);
+		this.setSize(900,900);
+		this.setResizable(false);
+		// panel = new Panel(222,242,controladorJuego);
+		panel = new Panel(666,726,controladorJuego);
+		this.add(panel);
+		
+		Button boton = new Button();
+		boton.setLabel("Boton");
+		this.add(boton);
+		
+		this.setBackground(Color.black);
+		panel.setBackground(Color.black);
+		
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				dispose();
+				System.exit(0);
+			}
+		});
+	}
+
+	private ControladorJuego controladorJuego;
+	private static final long serialVersionUID = 1L;
+	private Panel panel;
+	
+	public SuperficieDeDibujo getSuperficieDeDibujo() {
+		return this.panel;
+	}
+}
