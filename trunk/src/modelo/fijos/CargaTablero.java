74bb6586515bf64f3b3c105fed9c3f098acd771c
mrodriguez861 <mrodriguez861@91591542-cae0-11de-b743-054a4f195298>
2009-12-08 23:43:22 +0000

    Agrege las casas a los tableros

--- trunk/src/modelo/fijos/CargaTablero.java
+++ trunk/src/modelo/fijos/CargaTablero.java
@@ -23,6 +23,8 @@ public class CargaTablero {
 		Punto posicion;
 		Tablero tablero;
 		File file = new File(path);
+		int s = 0;
+		int i = 0;
 		try {
 			tablero = new Tablero(32,32);
 			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
@@ -30,13 +32,14 @@ public class CargaTablero {
 			Document doc = db.parse(file);
 			doc.getDocumentElement().normalize();
 			NodeList nodeLst = doc.getElementsByTagName("fila");
-			for ( int s = 0; s < nodeLst.getLength(); s++){
+			
+			for ( s = 0; s < nodeLst.getLength(); s++){
 				Node fstNode = nodeLst.item(s);
 				if ( fstNode.getNodeType() == Node.ELEMENT_NODE){
 					
 					Element fstElmnt = (Element) fstNode;
 					NodeList fstNmElemLst = fstElmnt.getElementsByTagName("casillero");
-					for ( int i = 0; i < fstNmElemLst.getLength(); i++){
+					for (  i = 0; i < fstNmElemLst.getLength(); i++){
 						Element fir = (Element) fstNmElemLst.item(i);
 						NodeList fstNm = fir.getChildNodes();
 						posicion = new Punto(i, s);
@@ -47,6 +50,7 @@ public class CargaTablero {
 							casilleroAux = new Pared(posicion, tablero);
 						}else if (tipoDeCasillero.equals("casa")){
 							casilleroAux = new Casa(posicion, tablero);
+							tablero.agregarCasa(posicion);
 						}else if (tipoDeCasillero.equals("PuntoDePoder")){
 							casilleroAux = new PuntoDePoder(posicion,tablero);
 						}
@@ -54,8 +58,10 @@ public class CargaTablero {
 						// System.out.println("Punto X = " + s +" Punto Y = "+ i + " Casillero = " + ((Node)fstNm.item(0)).getNodeValue());
 						// System.out.println(tab.esTransitable(nuevo));
 					}
-			   }
+			    }
 	    	}
+		    Punto nuevaDimension = new Punto(s, i);
+		    tablero.setDimension(nuevaDimension);
 			return tablero;
 		}
 		catch(Exception e){
