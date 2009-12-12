355727d19fcfcf36ed5bb1610588075558e5374b
elfede521 <elfede521@91591542-cae0-11de-b743-054a4f195298>
2009-12-12 19:27:23 +0000

    Las vistas de los casilleros ahora se cargan desde el objeto tablero en memoria Las semillas y los ptos de poder cambian de color cuando el pac se las come

--- trunk/src/modelo/vista/fijos/VistaCasillero.java
+++ trunk/src/modelo/vista/fijos/VistaCasillero.java
@@ -2,6 +2,8 @@ package modelo.vista.fijos;
 
 import java.awt.Color;
 
+import modelo.fijos.Casillero;
+
 import ar.uba.fi.algo3.titiritero.vista.Cuadrado;
 
 public class VistaCasillero extends Cuadrado {
