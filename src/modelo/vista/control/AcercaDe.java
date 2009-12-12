c2989c9ed43f3603e80caba8addbeee72d02c126
julian20dambrosio <julian20dambrosio@91591542-cae0-11de-b743-054a4f195298>
2009-12-12 03:37:51 +0000

    Creacion de menus

---
+++ src/modelo/vista/control/AcercaDe.java
@@ -0,0 +1,70 @@
+package modelo.vista.control;
+
+import java.awt.Color;
+import java.awt.Dialog;
+import java.awt.Frame;
+import java.awt.TextArea;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class AcercaDe extends Dialog {
+
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public AcercaDe(Frame unaVentana, String unString){
+		
+		super(unaVentana,unString);
+		
+		this.setVisible(true);
+		this.setSize(220,220);
+		this.setLocation(250,200);
+		this.escribirTexto();
+	
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				dispose();
+				//System.exit(0);
+			}
+		
+		});
+		
+		
+	}
+	public void escribirTexto(){
+		
+		
+		TextArea area = new TextArea();
+	
+		area.setBackground(Color.WHITE);
+		
+				
+		String informaciónDeCreación;
+		
+		informaciónDeCreación = "Nombre del Juego: \n\t AlgoIII - Man \n" +
+			
+								"\n Autores:\n\t" +
+								"Palladoro Federico \n\t" + 
+								"Marcelo Rodriguez \n\t" +
+								"DAmbrosio Julian \n" + 
+								
+								"\n Fecha de creacion: \n\t 12/2009";
+			
+				
+		
+		
+		area.setText(informaciónDeCreación);
+		area.setEditable(false);
+		
+		
+		this.add(area);
+	
+	}
+	
+}
+	
+
+
