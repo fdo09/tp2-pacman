9d7385a567564f331d743821ab789829dcf29eaf
elfede521 <elfede521@91591542-cae0-11de-b743-054a4f195298>
2009-12-13 21:06:10 +0000

    Cargue una imagen nueva para el menu principal

--- trunk/src/modelo/vista/control/VistaPrincipal.java
+++ trunk/src/modelo/vista/control/VistaPrincipal.java
@@ -6,9 +6,9 @@ import ar.uba.fi.algo3.titiritero.vista.Imagen;
 public class VistaPrincipal extends Imagen {
 
 	public VistaPrincipal(){
-		String nombreArchivoImagen = "inicio.jpg";
+		String nombreArchivoImagen = "inicial.jpg";
 		setNombreArchivoImagen(nombreArchivoImagen);
-		Punto posicionImagen = new Punto (5*25, 0);
+		Punto posicionImagen = new Punto (0, 0);
 		setPosicionable(posicionImagen);
 	}
 
