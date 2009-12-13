c3d15d39b3a9c6ecf929acc5f1bc23cb4871488b
elfede521 <elfede521@91591542-cae0-11de-b743-054a4f195298>
2009-12-13 16:52:26 +0000

    Arrgele la vistaVidas para que se vea la cantidad de vidas que hay actualmente

--- trunk/src/modelo/vista/fijos/VistaVidas.java
+++ trunk/src/modelo/vista/fijos/VistaVidas.java
@@ -1,6 +1,10 @@
 package modelo.vista.fijos;
 
+import java.awt.Graphics;
+
+import modelo.fijos.Juego;
 import modelo.fijos.Punto;
+import ar.uba.fi.algo3.titiritero.SuperficieDeDibujo;
 import ar.uba.fi.algo3.titiritero.vista.Imagen;
 
 public class VistaVidas extends Imagen{
@@ -12,6 +16,13 @@ public class VistaVidas extends Imagen{
 		setPosicionable(posicionVidas);
 	}
 	
-	
+	public void dibujar(SuperficieDeDibujo superficeDeDibujo) {
+		int vidas = Juego.getInstancia().getJugador().getCantVidas();
+		Graphics grafico = (Graphics)superficeDeDibujo.getBuffer();
+		for (int i = 1; i <= vidas; i++){
+			grafico.drawImage(super.getImagen(), super.getPosicionable().getX(), 
+					super.getPosicionable().getY() + i*25, null);
+		}
+	}
 	
 }
