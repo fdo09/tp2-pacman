6cf02fc666ff73e0ad9cfedb72e2dd5f7b21b000
julian20dambrosio <julian20dambrosio@91591542-cae0-11de-b743-054a4f195298>
2009-12-16 22:27:59 +0000

    Correciones

--- trunk/src/modelo/fijos/AdministradorJuego.java
+++ trunk/src/modelo/fijos/AdministradorJuego.java
@@ -4,6 +4,8 @@ import modelo.vista.control.VistaMensaje;
 import ar.uba.fi.algo3.titiritero.ControladorJuego;
 import ar.uba.fi.algo3.titiritero.ObjetoVivo;
 
+
+
 public class AdministradorJuego implements ObjetoVivo{
 	
 	private Tablero tablero;
@@ -20,12 +22,30 @@ public class AdministradorJuego implements ObjetoVivo{
 	}
 
 	private void controlarNivel() {
+		
+			
 		if (this.tablero.getSemillasRestantes() == 0){
+			
+			Juego.cambiarEstadoDelJuego(EstadosJuego.GANADO);
+			
 			VistaMensaje nuevoNivel = new VistaMensaje("Felicidades ha avanzado de nivel");
+			
 			controlador.agregarDibujable(nuevoNivel);
+			
 			Juego.getInstancia().aumentarNivel();
+		}
+		else if(Juego.getInstancia().getJugador().getCantVidas() == 0){
+						
+			Juego.cambiarEstadoDelJuego(EstadosJuego.PERDIDO);
+			
+			VistaMensaje juegoPerdido = new VistaMensaje("Game Over");
+			
+			controlador.agregarDibujable(juegoPerdido);
+			
 		}
 		
 	}
+
+	
 
 }
