2a21986b2aa49a1abbf86640f1f4f70798798e0f
julian20dambrosio <julian20dambrosio@91591542-cae0-11de-b743-054a4f195298>
2009-12-16 21:49:28 +0000

    Refactorizacion

---
+++ trunk/src/modelo/vista/control/CargaVistaPersonajes.java
@@ -0,0 +1,77 @@
+package modelo.vista.control;
+
+import java.util.ArrayList;
+
+import modelo.fijos.Tablero;
+import modelo.moviles.Fantasma;
+import modelo.moviles.FantasmaAmarillo;
+import modelo.moviles.FantasmaAzul;
+import modelo.moviles.FantasmaInmune;
+import modelo.moviles.FantasmaNaranja;
+import modelo.moviles.FantasmaRojo;
+import modelo.moviles.Pacman;
+import modelo.vista.moviles.VistaFantasmaAmarillo;
+import modelo.vista.moviles.VistaFantasmaAzul;
+import modelo.vista.moviles.VistaFantasmaInmune;
+import modelo.vista.moviles.VistaFantasmaNaranja;
+import modelo.vista.moviles.VistaFantasmaRojo;
+import modelo.vista.moviles.VistaPacman;
+import ar.uba.fi.algo3.titiritero.ControladorJuego;
+
+public class CargaVistaPersonajes {
+
+	public static void cargaVistas(ControladorJuego controlador, Tablero tablero){
+		generarVistaPacman(controlador, tablero);
+		generarVistaFantasmas(controlador, tablero);
+		
+	}
+
+	private static void generarVistaFantasmas(ControladorJuego controlador,
+			Tablero tablero) {
+		ArrayList<Fantasma> fantasmas = tablero.getFantasmas();
+		for(Fantasma f : fantasmas){
+			if(f.getClass().equals(FantasmaRojo.class)){
+				VistaFantasmaRojo vistaRojo = new VistaFantasmaRojo(f);
+				vistaRojo.setPosicionable(f);
+				controlador.agregarDibujable(vistaRojo);
+			}
+			else if(f.getClass().equals(FantasmaAmarillo.class)){
+				VistaFantasmaAmarillo vistaAmarillo = new VistaFantasmaAmarillo(f);
+			    vistaAmarillo.setPosicionable(f);
+			    controlador.agregarDibujable(vistaAmarillo);
+			}
+			else if(f.getClass().equals(FantasmaInmune.class)){
+				VistaFantasmaInmune vistaInmune = new VistaFantasmaInmune(f);
+			    vistaInmune.setPosicionable(f);
+			    controlador.agregarDibujable(vistaInmune);
+			}
+			else if(f.getClass().equals(FantasmaNaranja.class)){
+				VistaFantasmaNaranja vistaNaranja = new VistaFantasmaNaranja(f);
+				vistaNaranja.setPosicionable(f);
+				controlador.agregarDibujable(vistaNaranja);
+			}
+			else if(f.getClass().equals(FantasmaAzul.class)){
+				VistaFantasmaAzul vistaAzul = new VistaFantasmaAzul(f);
+				vistaAzul.setPosicionable(f);
+				controlador.agregarDibujable(vistaAzul);
+			}
+			controlador.agregarObjetoVivo(f);
+		
+		}
+		
+	}
+
+
+
+	private static void generarVistaPacman(ControladorJuego controlador,
+			Tablero tablero) {
+		
+		Pacman pacman = tablero.getPacman();
+		VistaPacman vistaPacman = new VistaPacman(pacman);
+		vistaPacman.setPosicionable(pacman);;
+		
+		controlador.agregarObjetoVivo(pacman);
+		controlador.agregarDibujable(vistaPacman);
+		
+	}
+}
--- trunk/src/modelo/vista/control/CargaVistaTablero.java
+++ trunk/src/modelo/vista/control/CargaVistaTablero.java
@@ -1,7 +1,6 @@
 package modelo.vista.control;
 
 import java.awt.Color;
-import java.util.ArrayList;
 
 import modelo.fijos.Casa;
 import modelo.fijos.Casillero;
@@ -10,25 +9,12 @@ import modelo.fijos.Punto;
 import modelo.fijos.PuntoDePoder;
 import modelo.fijos.Semilla;
 import modelo.fijos.Tablero;
-import modelo.moviles.Fantasma;
-import modelo.moviles.FantasmaAmarillo;
-import modelo.moviles.FantasmaAzul;
-import modelo.moviles.FantasmaInmune;
-import modelo.moviles.FantasmaNaranja;
-import modelo.moviles.FantasmaRojo;
-import modelo.moviles.Pacman;
 import modelo.vista.fijos.VistaBarraDeDatos;
 import modelo.vista.fijos.VistaCasillero;
 import modelo.vista.fijos.VistaPuntoDePoder;
 import modelo.vista.fijos.VistaPuntos;
 import modelo.vista.fijos.VistaSemilla;
 import modelo.vista.fijos.VistaVidas;
-import modelo.vista.moviles.VistaFantasmaAmarillo;
-import modelo.vista.moviles.VistaFantasmaAzul;
-import modelo.vista.moviles.VistaFantasmaInmune;
-import modelo.vista.moviles.VistaFantasmaNaranja;
-import modelo.vista.moviles.VistaFantasmaRojo;
-import modelo.vista.moviles.VistaPacman;
 import ar.uba.fi.algo3.titiritero.ControladorJuego;
 
 public class CargaVistaTablero {
@@ -38,10 +24,7 @@ public class CargaVistaTablero {
 
 	}
 	public static void cargaVistas(ControladorJuego controlador, Tablero tablero){
-		Pacman pacman = tablero.getPacman();
-		controlador.agregarKeyPressObservador(new EscuchadorDeKeyPress(pacman));
 		generarVistaDeCasilleros(controlador, tablero);
-		generarVistaDePersonajes(controlador, tablero);
 		generarVistaDeDatos(controlador, tablero);
 	}
 	
@@ -60,48 +43,7 @@ public class CargaVistaTablero {
 		
 	}
 
-	private static void generarVistaDePersonajes(ControladorJuego controlador,
-			Tablero tablero) {
-		ArrayList<Fantasma> fantasmas = tablero.getFantasmas();
-		for(Fantasma f : fantasmas){
-			if(f.getClass().equals(FantasmaRojo.class)){
-				VistaFantasmaRojo vistaRojo = new VistaFantasmaRojo(f);
-				vistaRojo.setPosicionable(f);
-				controlador.agregarDibujable(vistaRojo);
-			}
-			else if(f.getClass().equals(FantasmaAmarillo.class)){
-				VistaFantasmaAmarillo vistaAmarillo = new VistaFantasmaAmarillo(f);
-			    vistaAmarillo.setPosicionable(f);
-			    controlador.agregarDibujable(vistaAmarillo);
-			}
-			else if(f.getClass().equals(FantasmaInmune.class)){
-				VistaFantasmaInmune vistaInmune = new VistaFantasmaInmune(f);
-			    vistaInmune.setPosicionable(f);
-			    controlador.agregarDibujable(vistaInmune);
-			}
-			else if(f.getClass().equals(FantasmaNaranja.class)){
-				VistaFantasmaNaranja vistaNaranja = new VistaFantasmaNaranja(f);
-				vistaNaranja.setPosicionable(f);
-				controlador.agregarDibujable(vistaNaranja);
-			}
-			else if(f.getClass().equals(FantasmaAzul.class)){
-				VistaFantasmaAzul vistaAzul = new VistaFantasmaAzul(f);
-				vistaAzul.setPosicionable(f);
-				controlador.agregarDibujable(vistaAzul);
-			}
-			controlador.agregarObjetoVivo(f);
 		
-		}
-		
-		Pacman pacman = tablero.getPacman();
-		VistaPacman vistaPacman = new VistaPacman(pacman);
-		vistaPacman.setPosicionable(pacman);;
-		
-		controlador.agregarObjetoVivo(pacman);
-		controlador.agregarDibujable(vistaPacman);
-		
-	}
-	
 	private static void generarVistaDeCasilleros(ControladorJuego controlador, Tablero tablero) {
 		Punto dimension = tablero.getDimension();
 		for ( int x = 0; x < dimension.getPuntoX(); x++ ){
